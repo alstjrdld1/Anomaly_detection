e3656e60a3910aadce7bc96f2ffb8a25cf8a6084
alstjrdld1 <minseok_kim@khu.ac.kr>
2022-05-19 18:19:39 +0900

    20220519

--- code/Train_with_stacked_data.py
+++ code/Train_with_stacked_data.py
@@ -321,6 +321,7 @@ def train(train_loader, epoch, model, optimizer, criterion):
     for i, (input, target) in enumerate(train_loader):
         # measure data loading time 
         data_time.update(time.time() - end)
+        print("input shape : ", input.shape)
         input = np.kron(input, np.ones((7, 7)))
                 
         input = torch.tensor(input, dtype=torch.float32)
